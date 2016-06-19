67fb6e58a8662156aa8486c1461c80fefa8946b8
Tirtha Ghosh <tirtha15>
2016-06-18 17:04:23 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clickClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clickClientApp
+ */
+angular.module('clickClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clickClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clickClientApp
+ */
+angular.module('clickClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
